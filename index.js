028dc640b33165f4840af034303fa6f63ad475b9
MdMehediHasan22 <mdmehedi3799@gmail.com>
2025-11-06 15:38:41 +0600

    backend

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {mongodb,ServerApiVersion} = require('mongodb');
+const {MongoClient,ServerApiVersion} = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://mdmehedi3799:Nasa%4020201@cluster0.2pip15y.mongodb.net/?appName=Cluster0";
+const uri = "mongodb+srv://mdmehedi3799:Nasa20201@cluster0.2pip15y.mongodb.net/?appName=Cluster0";
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -22,6 +22,12 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.post('/users',(req,res) =>{
+      const user = req.body;
+      console.log("data in the server",user);
+      
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
