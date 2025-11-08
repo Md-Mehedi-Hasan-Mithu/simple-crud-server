3e610b4420040aaea7b0117f7885218d3abf8b27
MdMehediHasan22 <mdmehedi3799@gmail.com>
2025-11-08 10:53:31 +0600

    data successfully added in the database

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const {MongoClient,ServerApiVersion} = require('mongodb');
 const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
-
 const uri = "mongodb+srv://mdmehedi3799:Nasa20201@cluster0.2pip15y.mongodb.net/?appName=Cluster0";
-
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -17,23 +16,36 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
+let userCollection;
+let studentCollection;
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    console.log("MongoDB connected successfully!");
+    const dbUser = client.db('usrersdb');
+    userCollection = dbUser.collection('users');
+    const dbStudent = client.db('studentsdb');
+    studentCollection = dbStudent.collection('students');
 
-    app.post('/users',(req,res) =>{
-      const user = req.body;
-      console.log("data in the server",user);
-      
+
+    app.post('/users',async(req,res) =>{
+      const newUser = req.body;
+      console.log("data in the server",newUser);
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+    app.post('/students',async(req,res) =>{
+      const newStudent = req.body;
+      console.log("data in the server",newStudent);
+      const resultStd = await studentCollection.insertOne(newStudent);
+      res.send(resultStd);
     });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    
   }
 }
 run().catch(console.dir);
@@ -41,7 +53,14 @@ run().catch(console.dir);
 app.get('/', (req, res) => {
   res.send('Simple Crud server Running');
 });
-
+app.get('/users', async (req, res) => {
+  const users = await userCollection.find().toArray();
+  res.send(users);
+});
+app.get('/students', async (req, res) => {
+  const students = await studentCollection.find().toArray();
+  res.send(students);
+});
 app.listen(port,() =>{
     console.log(`Simple crud server running on port ${port} `)
 });
